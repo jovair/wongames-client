9fce3e4fc380ea1d48584425f51713c520c368f6
Jovair <jvr_ribeiro@live.com>
2022-05-29 15:36:24 -0300

    Componente BannerSlider sem CSS

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,28 @@
+import React, { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
+import * as S from './styles'
+
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLAnchorElement>
+
+export type ButtonProps = {
+  size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
+  icon?: JSX.Element
+  as?: React.ElementType
+} & ButtonTypes
+
+const Button = ({
+  children,
+  icon,
+  size = 'medium',
+  fullWidth = false,
+  ...props
+}: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+    {!!icon && icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
+)
+
+export default Button
