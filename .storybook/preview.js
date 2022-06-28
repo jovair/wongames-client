c876c7985f87626daac6c72ce544a729965d9e5d
Jovair <jvr_ribeiro@live.com>
2022-06-27 22:16:43 -0300

    Componente Container finalizado

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,19 +1,33 @@
 import { ThemeProvider } from 'styled-components'
 import { RouterContext } from 'next/dist/shared/lib/router-context'
 import GlobalStyles from 'styles/global'
-
 import theme from 'styles/theme'
+
+// .storybook/preview.js
 
 export const parameters = {
   nextRouter: {
     Provider: RouterContext.Provider
+  },
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
   }
 }
 
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      <GlobalStyles removeBg />
       <Story />
     </ThemeProvider>
   )
--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -10,6 +10,6 @@ export const Default: Story<MenuProps> = (args) => <Menu {...args} />
 
 Default.parameters = {
   backgrounds: {
-    default: 'dark'
+    default: 'won-dark'
   }
 }
